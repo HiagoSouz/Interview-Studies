83cddebd490edd16375df467679b8d8ad21365cc
Hiago Souza <hiago.rafael.souza@hotmail.com>
2022-02-04 15:17:17 -0500

    Create IsPermutation.cpp

---
+++ IsPermutation.cpp
@@ -0,0 +1,35 @@
+/*
+
+Check Permutation: Given two strings, write a method to decide if one is a
+permutation of the other.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool isPermutation(string str1,string str2)
+{
+ 
+    if(str1.length()==str2.length())
+    {
+        sort(str1.begin(), str1.end());
+        sort(str2.begin(), str2.end());
+        
+        if(str1==str2)
+            return true;
+    }
+   
+   return false;
+}
+
+
+int main()
+{
+    bool result = isPermutation("blue","elub");
+    
+    cout << result << endl;
+}
