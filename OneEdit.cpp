be433fb88c3b9849b4518f52b26fd8c4e2221a89
Hiago Souza <hiago.rafael.souza@hotmail.com>
2022-02-06 15:12:31 -0500

    Update OneEdit.cpp

--- OneEdit.cpp
+++ OneEdit.cpp
@@ -23,11 +23,11 @@ bool OneAway(string s1, string s2){
    
     int m = s1.length(), n = s2.length();
  
-     if(n1-n2 > 1 || n2-n1 > 1)
+     if(m-n > 1 || n-m > 1)
             return false;
      
  
-    int count = 0; // Count of edits
+    int count = 0; 
  
     int i = 0, j = 0;
     while (i < m && j < n)
@@ -61,9 +61,11 @@ bool OneAway(string s1, string s2){
  
     if (i < m || j < n)
         count++;
- 
-    return count == 1;
-    
+        
+    if(count==1)
+        return true;
+    else
+        return false;
 }
 
 int main()
