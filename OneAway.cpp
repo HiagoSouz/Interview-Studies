7bc38a7318cee16dc92d463116c4404e43f1717b
Hiago Souza <hiago.rafael.souza@hotmail.com>
2022-02-06 14:17:28 -0500

    Create OneAway.cpp

---
+++ OneAway.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool OneAway(string s1, string s2){
+    
+    int n1 = s1.length();
+    int n2 = s2.length();
+    int m;
+    int count = 0;
+    
+    if(n1>n2)
+        m = n1;
+    
+    else 
+        m = n2;
+     
+    if(n1!=n2){
+        
+        if(n1-n2 > 1 || n2-n1 > 1){
+            return false;
+        }
+        
+        
+    }
+    
+    for(int i=0;i<m;i++){
+        if(s1[i]!=s2[i])
+            count++;
+            
+        if(count>1){
+            return false;
+        }
+    }
+    
+    return true;
+    
+}
+
+int main()
+{
+    string s1 = "teste1";
+    string s2 = "teste2";
+    cout << OneAway(s1,s2);
+    return 0;
+}
