160aa9129c12455cf972ea4d3c161a008aabd4c8
Hiago Souza <hiago.rafael.souza@hotmail.com>
2022-02-18 10:51:16 -0500

    Create SearchInsert.cpp

---
+++ SearchInsert.cpp
@@ -0,0 +1,15 @@
+/*
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+*/ 
+ 
+int searchInsert(vector<int>& nums, int target) {
+    for(int i=0;i<nums.size();i++)
+      if(nums[i]>target || nums[i]==target)
+          return i;
+        
+        return nums.size();
+        
+}
